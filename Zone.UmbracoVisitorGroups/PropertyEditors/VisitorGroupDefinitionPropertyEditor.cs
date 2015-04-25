5a4e28b935cf0704d13d3d30cc397be12ac2cdb5
Andy Butland <abutland@zonecontent.com>
2015-04-25 21:06:31 +0200

    Removed the dummy property editors now demonstrated working for extensions in other assemblies

--- Zone.UmbracoVisitorGroups/PropertyEditors/VisitorGroupDefinitionPropertyEditor.cs
+++ Zone.UmbracoVisitorGroups/PropertyEditors/VisitorGroupDefinitionPropertyEditor.cs
@@ -10,6 +10,8 @@
     /// </summary>
     [PropertyEditor(Constants.VisitorGroupDefinitionPropertyEditorAlias, "Visitor group definition", "/App_Plugins/UmbracoVisitorGroups/Resource/editor.html", ValueType = "JSON")]
     [PropertyEditorAsset(ClientDependencyType.Javascript, "/App_Plugins/UmbracoVisitorGroups/Resource/editor.controller.js")]
+    [PropertyEditorAsset(ClientDependencyType.Javascript, "/App_Plugins/UmbracoVisitorGroups/ResourceForCriteria/dayOfWeek/definition.editor.controller.js")]
+    [PropertyEditorAsset(ClientDependencyType.Javascript, "/App_Plugins/UmbracoVisitorGroups/ResourceForCriteria/timeOfDay/definition.editor.controller.js")]
     public class VisitorGroupDefinitionPropertyEditor : PropertyEditor
     {
     }
