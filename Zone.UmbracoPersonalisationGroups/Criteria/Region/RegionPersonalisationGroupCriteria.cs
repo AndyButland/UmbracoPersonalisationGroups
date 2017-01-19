2353fcf83cc9b9d9faf22d84aa7c0d2be911a38f
Andy Butland <abutland@thisiszone.com>
2017-01-19 17:42:38 +0000

    Removed test IPs

--- Zone.UmbracoPersonalisationGroups/Criteria/Region/RegionPersonalisationGroupCriteria.cs
+++ Zone.UmbracoPersonalisationGroups/Criteria/Region/RegionPersonalisationGroupCriteria.cs
@@ -51,7 +51,6 @@
             }
 
             var ip = _ipProvider.GetIp();
-            ip = "62.252.197.226";
             if (!string.IsNullOrEmpty(ip))
             {
                 var country = _geoLocationProvider.GetCountryFromIp(ip);
