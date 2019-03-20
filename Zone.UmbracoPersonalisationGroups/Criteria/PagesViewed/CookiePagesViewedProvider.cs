77c58383151446682e9396bc639919f8fc3f32b0
Tom Waddell <twaddell@users.noreply.github.com>
2019-03-20 12:19:41 +0000

    Improvements for parsing of cookie values for PagesViewed

--- Zone.UmbracoPersonalisationGroups/Criteria/PagesViewed/CookiePagesViewedProvider.cs
+++ Zone.UmbracoPersonalisationGroups/Criteria/PagesViewed/CookiePagesViewedProvider.cs
@@ -13,12 +13,26 @@
             var cookie = HttpContext.Current.Request.Cookies[UmbracoConfig.For.PersonalisationGroups().CookieKeyForTrackingNumberOfVisits];
             if (!string.IsNullOrEmpty(cookie?.Value))
             {
-                return cookie.Value
-                    .Split(',')
-                    .Select(int.Parse);
+                return ParseCookieValue(cookie.Value);
             }
 
             return Enumerable.Empty<int>();
         }
+
+        public static List<int> ParseCookieValue(string cookieValue)
+        {
+            return cookieValue.Split(',')
+                              .Aggregate(new List<int>(),
+                                         (result, value) =>
+                                         {
+                                             if (int.TryParse(value, out var item))
+                                             {
+                                                 result.Add(item);
+                                             }
+
+                                             return result;
+                                         });
+        }
+        
     }
 }
