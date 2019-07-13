2d760025c43a18e6cbee9bfcce7c7c2392d1d88f
Andy Butland <abutland73@gmail.com>
2019-07-13 09:37:24 +0200

    Updated V8 referencing project to 8.1 Umbraco dependencies

--- Zone.UmbracoPersonalisationGroups.V8/PropertyValueConverter/PersonalisationGroupDefinitionPropertyValueConverter.cs
+++ Zone.UmbracoPersonalisationGroups.V8/PropertyValueConverter/PersonalisationGroupDefinitionPropertyValueConverter.cs
@@ -13,23 +13,23 @@
     /// </summary>
     public class PersonalisationGroupDefinitionPropertyValueConverter : PropertyValueConverterBase
     {
-        public override Type GetPropertyValueType(PublishedPropertyType propertyType)
+        public override Type GetPropertyValueType(IPublishedPropertyType propertyType)
             => typeof(PersonalisationGroupDefinition);
 
-        public override PropertyCacheLevel GetPropertyCacheLevel(PublishedPropertyType propertyType)
+        public override PropertyCacheLevel GetPropertyCacheLevel(IPublishedPropertyType propertyType)
             => PropertyCacheLevel.Element;
 
-        public override bool IsConverter(PublishedPropertyType propertyType)
+        public override bool IsConverter(IPublishedPropertyType propertyType)
         {
             return propertyType.EditorAlias.Equals(AppConstants.PersonalisationGroupDefinitionPropertyEditorAlias);
         }
 
-        public override object ConvertSourceToIntermediate(IPublishedElement owner, PublishedPropertyType propertyType, object source, bool preview)
+        public override object ConvertSourceToIntermediate(IPublishedElement owner, IPublishedPropertyType propertyType, object source, bool preview)
         {
             return source;
         }
 
-        public override object ConvertIntermediateToObject(IPublishedElement owner, PublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object inter, bool preview)
+        public override object ConvertIntermediateToObject(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object inter, bool preview)
         {
             if (inter == null)
             {
